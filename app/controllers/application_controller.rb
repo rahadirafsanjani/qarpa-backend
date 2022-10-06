acf3edde25078f0a67a6049688355a0bb022ccc3
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-10-06 17:18:09 +0700

    adding expiration in jwt

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::API
-  SECRET_KEY = Rails.application.credentials.jwt.token
+  SECRET_KEY = Rails.application.credentials.jwt.secret
   
   def encode_token(payload)
+    payload[:exp] = Time.now.to_i + 1 * 3600
     JWT.encode(payload, SECRET_KEY)
   end
 
