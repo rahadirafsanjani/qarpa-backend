c098b224319cfb074426e3eafcce37fe4af4f2f1
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-10-12 15:35:58 +0700

    create the migration and model for address

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_12_091123) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_12_083328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "companies", force: :cascade do |t|
-    t.string "name"
+  create_table "addresses", force: :cascade do |t|
+    t.string "full_address"
+    t.string "postal_code", limit: 5
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
