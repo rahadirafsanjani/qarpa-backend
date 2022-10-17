93dfc998809b06ec5ad32f49d1315531fab90225
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-10-17 15:29:59 +0700

    refactoring controller and model branch

--- app/models/branch.rb
+++ app/models/branch.rb
@@ -1,11 +1,15 @@
 class Branch < ApplicationRecord
+  attr_accessor :full_address, :postal_code
+  before_validation :create_address
+  
   belongs_to :address 
   belongs_to :company
-
+  
   validates :name, :fund, :notes, presence: true
   validates :fund, numericality: { only_integer: true }
+  
 
-  def close_branch()
+  def close_branch
     return false if self.status == false
 
     self.close_at = Time.now.utc
@@ -13,11 +17,16 @@ class Branch < ApplicationRecord
     save!
   end
 
-  def open_branch()
+  def open_branch
     return false if self.status == true
 
     self.open_at = Time.now.utc
     self.status = true
     save!
+  end
+
+  def create_address
+    address = Address.find_or_create_by(full_address: self.full_address, postal_code: self.postal_code)
+    self.address_id = address.id
   end
 end
