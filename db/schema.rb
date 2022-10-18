fdd55063c0eda94c7d61c1295610e864cb8c4328
rafsanjani rahadi <rahadirafsanjani@gmail.com>
2022-10-18 14:38:26 +0700

    resolve conflict 2

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,73 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_120117) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "addresses", force: :cascade do |t|
+    t.string "full_address"
+    t.string "postal_code", limit: 5
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "branches", force: :cascade do |t|
+    t.string "name"
+    t.datetime "open_at"
+    t.datetime "close_at"
+    t.boolean "status"
+    t.integer "fund"
+    t.string "notes"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "company_id"
+    t.bigint "address_id"
+    t.index ["address_id"], name: "index_branches_on_address_id"
+    t.index ["company_id"], name: "index_branches_on_company_id"
+  end
+
   create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "address_id"
+    t.index ["address_id"], name: "index_customers_on_address_id"
+  end
+
+  create_table "inventories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "company_id"
+    t.bigint "address_id"
+    t.index ["address_id"], name: "index_inventories_on_address_id"
+    t.index ["company_id"], name: "index_inventories_on_company_id"
+  end
+
+  create_table "inventory_products", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "inventory_id"
+    t.bigint "product_id"
+    t.index ["inventory_id"], name: "index_inventory_products_on_inventory_id"
+    t.index ["product_id"], name: "index_inventory_products_on_product_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.string "quantity_type"
+    t.string "category"
+    t.datetime "expire"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "supplier", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -31,6 +97,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_120117) do
     t.datetime "reset_password_sent_at"
     t.string "regist_token"
     t.datetime "regist_token_sent_at"
+    t.datetime "confirmed_at"
     t.bigint "company_id"
     t.index ["company_id"], name: "index_users_on_company_id"
   end
