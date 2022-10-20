c8bf625239186fe6a30ea6a26e337bb290ddc223
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-10-20 20:20:14 +0700

    create validation for permission and branch

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::OrdersController < ApplicationController
   before_action :authorize 
+  before_action :check_user_permission
+  before_action :check_branch_status
     
   def create 
     @order = Order.new(order_params)
@@ -10,6 +12,14 @@ class Api::V1::OrdersController < ApplicationController
 
   def response_to_json(message, status)
     render json: { message: message }, status: status
+  end
+
+  def check_user_permission 
+    response_to_json("You don't have permission to create new order", :forbidden) unless @user.branch.present?
+  end
+
+  def check_branch_status 
+    response_to_json("Please open the branch before do some order", :bad_request) unless @user.branch.status
   end
 
   def order_params 
