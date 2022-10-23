7677bc95d15365dc91ee6d56aa23d3e9f663b1f7
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-10-23 20:37:10 +0700

    refactor update password

--- app/controllers/api/v1/users/passwords_controller.rb
+++ app/controllers/api/v1/users/passwords_controller.rb
@@ -10,12 +10,12 @@ class Api::V1::Users::PasswordsController < ApplicationController
 
       render json: { message: "If an account with that email was found, we have sent a link to reset your password" }
     else
-      render json: { error: 'Email address not found. Please check and try again.' }, status: :not_found
+      render json: { message: 'Email address not found. Please check and try again.' }, status: :not_found
     end
   end
 
   def reset 
-    return render json: { error: 'Token not present' } if params[:token].blank?
+    return render json: { message: 'Token not present' } if params[:token].blank?
 
     token = params[:token].to_s 
 
@@ -25,19 +25,24 @@ class Api::V1::Users::PasswordsController < ApplicationController
       if user.reset_password!(params[:password])
         render json: { message: 'password updated successfully' }, status: :ok
       else
-        render json: { error: user.errors.full_messages }, status: :unprocessable_entity
+        render json: { message: user.errors.full_messages }, status: :unprocessable_entity
       end
     else
-      render json: { error: 'Link not valid or expired. Try generating a new link.' }, status: :not_found
+      render json: { message: 'Link not valid or expired. Try generating a new link.' }, status: :not_found
     end
   end
 
   def update
-    render json: { message: "Invalid username or address" } if !@user.authenticate(params[:old_password])
-    if @user.update(password: params[:new_password])
-      render json: { message: "password updated successfully" }, status: :ok
+    if @user.authenticate(params[:old_password]) && @user.update(password: params[:new_password])
+      response_to_json("Password updated successfully", :ok)
     else 
-      render json: @user.errors, status: :unprocessable_entity
+      response_to_json("Invalid password", :unprocessable_entity)
     end
+  end
+
+  private 
+
+  def response_to_json(message, status)
+    render json: { message: message }, status: status
   end
 end
