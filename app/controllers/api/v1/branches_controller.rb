a80e8f76e41ee4d2e7223b5be8c382af39c93a50
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-10-24 09:01:32 +0700

    refactor branch controller

--- app/controllers/api/v1/branches_controller.rb
+++ app/controllers/api/v1/branches_controller.rb
@@ -21,8 +21,8 @@ class Api::V1::BranchesController < ApplicationController
   end
 
   def open 
-    if @branch.open_branch() 
-      response_to_json("The branch has been opened", :ok) 
+    if @branch.open_branch
+      render json: { message: "The branch has been opened", data: @branch }, status: :ok
     else
       response_to_json("The branch is already opened", :ok) 
     end
@@ -31,11 +31,8 @@ class Api::V1::BranchesController < ApplicationController
   private 
 
   def set_branch
-    begin
-      @branch = Branch.find(params[:id])
-    rescue => exception
-      response_to_json("Branch not found", :not_found)
-    end
+    @branch = Branch.find_by(id: params[:id])
+    response_to_json("Branch not found", :not_found) if @branch.blank?
   end
 
   def response_to_json(message, status) 
