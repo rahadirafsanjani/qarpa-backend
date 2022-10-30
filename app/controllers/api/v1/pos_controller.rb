8055e19cd3c6984ec4381f507e6ad0a3376e60a0
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-10-30 12:51:45 +0700

    change the response url and response body for pos

--- app/controllers/api/v1/pos_controller.rb
+++ app/controllers/api/v1/pos_controller.rb
@@ -3,13 +3,13 @@ class Api::V1::PosController < ApplicationController
   before_action :set_pos, only: %i[ close ]
 
   def open 
-    @pos = Pos.new(pos_params)
+    @pos = Pos.new(pos_params.merge(branch_id: params[:id]))
     @pos.save ? response_to_json("New pos has been opened", @pos, :created) : 
                 response_error(@pos.errors, :unprocessable_entity)
   end
 
   def close 
-    @pos.close_pos ? response_to_json("Pos has been closed", @pos, :ok) :
+    @pos.close_pos ? response_to_json("Pos has been closed", @pos.new_response, :ok) :
                      response_error("Something went wrong", :unprocessable_entity)
   end
 
@@ -29,6 +29,6 @@ class Api::V1::PosController < ApplicationController
   end
 
   def pos_params 
-    params.require(:pos).permit(:fund, :notes, :branch_id).merge(user_id: @user.id) 
+    params.require(:pos).permit(:fund, :notes).merge(user_id: @user.id) 
   end
 end
