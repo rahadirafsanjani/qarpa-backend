49dcff686651134fbf132b205e15d88aa3c5f563
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-10-31 21:06:47 +0700

    change the response in customer controller

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -4,30 +4,30 @@ class Api::V1::CustomersController < ApplicationController
 
   def index 
     @customers = Customer.customer_response(company_id: @user.company_id)
-    response_to_json(@customers, :ok)
+    response_to_json("Customers", @customers, :ok)
   end
 
   def create 
     @customer = Customer.new(customer_params)
     
-    @customer.save ? response_to_json(@customer.new_response, :created) : 
+    @customer.save ? response_to_json("New customer created", @customer.new_response, :created) : 
                      response_error(@customer.errors, :unprocessable_entity)
   end
 
   def update 
-    @customer.update(customer_params) ? response_to_json(@customer.new_response, :ok) :
+    @customer.update(customer_params) ? response_to_json("Customer updated", @customer.new_response, :ok) :
                                         response_error(@customer.errors, :unprocessable_entity) 
   end
 
   def destroy
-    @customer.destroy ? response_to_json(@customer.new_response, :ok) :
+    @customer.destroy ? response_to_json("Customer deleted", @customer.new_response, :ok) :
                         response_error("Something went wrong", :unprocessable_entity)
   end
 
   private 
 
-  def response_to_json(message, status)
-    render json: message, status: status
+  def response_to_json(message, data, status)
+    render json: { message: message, data: data }, status: status
   end
 
   def response_error(message, status)
