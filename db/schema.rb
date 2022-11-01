1b7d2b8e1d2e13c8da714253dfcd4ec5dc5ff91e
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-11-01 14:04:12 +0700

    add new field discount to order table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_01_055638) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_01_063935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -147,6 +147,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_01_055638) do
     t.datetime "updated_at", null: false
     t.bigint "customer_id"
     t.bigint "pos_id"
+    t.integer "discount"
     t.index ["customer_id"], name: "index_orders_on_customer_id"
     t.index ["pos_id"], name: "index_orders_on_pos_id"
   end
