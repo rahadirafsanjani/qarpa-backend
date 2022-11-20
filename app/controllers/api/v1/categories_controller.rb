6bfe4fddc072443d649374fdeb232f65f2dcd572
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-11-20 21:58:24 +0700

    change model get categories

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::CategoriesController < ApplicationController
   before_action :authorize
 
   def index
-    @categories = Category.all 
+    @categories = Category.get_categories
     response_to_json("List categories", @categories, :ok)
   end
 end
