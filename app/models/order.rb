41284a6c261620451d9cb495cb6df97a52e67f50
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-11-20 21:44:08 +0700

    fix order models and controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -17,8 +17,8 @@ class Order < ApplicationRecord
 
     self.items.each do |item|
       @products.each do |product|
-        if product.id == item[:product_id] && product.quantity < item[:qty]
-          errors.add(:qty, "#{product.name} not enough stock")
+        if product.id == item[:product_shared_id] && product.qty < item[:qty]
+          errors.add(:qty, "#{product.product.name} not enough stock")
           valid = valid + 1
         end
       end 
@@ -29,22 +29,21 @@ class Order < ApplicationRecord
 
   def reduce_stock 
     self.detail_orders.each do |detail_order|
-        detail_order.product.quantity = detail_order.product.quantity - detail_order.qty
-        detail_order.product.save!(validate: false)
+        detail_order.product_shared.qty = detail_order.product_shared.qty - detail_order.qty
+        detail_order.product_shared.save!(validate: false)
     end
   end
 
   def get_products 
-    @products = Product.where(id: get_product_id)
+    @products = ProductShared.where(id: get_product_id)
+    binding.pry
   end
 
   def get_product_id 
-    return self.items.map do |item|
-      item[:product_id]
-    end
+    return self.items.map { |item| item[:product_shared_id] }
   end
 
   def create_detail_orders
-    self.detail_orders.insert_all(self.items)
+    self.detail_orders.create(self.items)
   end
 end
