2a5862e01298a23f8caae5bc4fd3770f707a1ec4
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-11-20 23:13:39 +0700

    change parameters for dropdown supplier params

--- app/controllers/api/v1/suppliers_controller.rb
+++ app/controllers/api/v1/suppliers_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SuppliersController < ApplicationController
   before_action :authorize
 
   def dropdown
-    @suppliers = Supplier.dropdown
+    @suppliers = Supplier.dropdown(company_id: @user.company_id)
     response_to_json("Suppliers dropdown", @suppliers, :ok)
   end
 end
--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -2,8 +2,8 @@ class Supplier < ApplicationRecord
   has_many :products, through: :product_shared
   has_many :product_shared
 
-  def self.dropdown
-    @suppliers = Supplier.all
+  def self.dropdown params = {}
+    @suppliers = Supplier.where(company_id: params[:company_id])
     @suppliers.map do |supplier|
       supplier.dropdown_attribute
     end
