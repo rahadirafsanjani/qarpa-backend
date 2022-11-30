5f8b5470ff3054398bfda82a48d6f164389a2d89
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-11-30 23:22:49 +0700

    create new method get reports and expenses incomes

--- app/controllers/api/v1/audit_controller.rb
+++ app/controllers/api/v1/audit_controller.rb
@@ -4,16 +4,11 @@ class Api::V1::AuditController < ApplicationController
   def sum_expenses_incomes 
     @sum = Audit.expenses_incomes(company_id: @user.company_id)
     response_to_json("Incomes and expenses", @sum, :ok)
-    # response_to_json("Incomes and expenses", { incomes: 1000000, expenses: 500000 }, :ok)
   end
 
   def reports 
     @reports = Audit.get_reports(company_id: @user.company_id, date: params[:date])
-    
-    response = {}
-    response.merge!(messages: "Report list")
-    response.merge!(@reports)
 
-    render json: response, status: :ok
+    response_to_json("Reports list", @reports, :ok)
   end
 end
